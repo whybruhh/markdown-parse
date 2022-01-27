cdd19a8b42df459da9db4a9fc274e8080555a9ae
whybruhh <57416121+whybruhh@users.noreply.github.com>
2022-01-27 10:41:10 -0800

    Write four test for my break file

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
@@ -13,10 +14,38 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("./test-file.md"));
-        List<String> expect = List.of("https://something.com", "some-page.html");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void getLinks() throws IOException {
+        String contents = Files.readString(Path.of("./test-file.md"));
+        List<String> expectLink = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expectLink);
     }
-    
+
+    @Test
+    public void testbreakFile() throws IOException {
+        String contents = Files.readString(Path.of("./break-file.md"));
+        List<String> expectLink = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expectLink);
+    }
+
+    @Test
+    public void testbreakFileTwo() throws IOException {
+        String contents = Files.readString(Path.of("./break-file2.md"));
+        List<String> expectLink = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expectLink);
+    }
+
+    @Test
+    public void testbreakFileThree() throws IOException {
+        String contents = Files.readString(Path.of("./break-file3.md"));
+        List<String> expectLink = List.of("hello.com");
+        assertEquals(MarkdownParse.getLinks(contents), expectLink);
+    }
+
+    @Test
+    public void testbreakFileFour() throws IOException {
+        String contents = Files.readString(Path.of("./break-file4.md"));
+        ArrayList<String> emptyList = new ArrayList<String>();
+        assertEquals(MarkdownParse.getLinks(contents), emptyList);
+    }
+
 }
