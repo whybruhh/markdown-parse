b82cf8c842801ef446c1952a8db661ba9440b7a7
whybruhh <57416121+whybruhh@users.noreply.github.com>
2022-02-25 15:19:11 -0800

    change for lab

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -33,11 +33,11 @@ public class MarkdownParse {
                     toReturn.add(markdown.substring(openParen + 1, closeParen));
                     currentIndex = closeParen + 1;
                 } else {
-                    return null;
+                    break;
                 }
 
             } else {
-                return null;
+                break;
             }
         }
         return toReturn;
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,44 +8,57 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class MarkdownParseTest {
+
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void testSnippet1() throws IOException {
+        String contents = Files.readString(Path.of("./Snippet1.md"));
+        List<String> expectLink = List.of("`google.com", "google.com", "ucsd.edu");
+        assertEquals(expectLink, MarkdownParse.getLinks(contents));
     }
 
     @Test
-    public void getLinks() throws IOException {
-        String contents = Files.readString(Path.of("./test-file.md"));
-        List<String> expectLink = List.of("https://something.com", "some-page.html");
-        assertEquals(MarkdownParse.getLinks(contents), expectLink);
+    public void testSnippet2() throws IOException {
+        String contents = Files.readString(Path.of("./Snippet2.md"));
+        List<String> expectLink = List.of("a.com", "a.com(())", "example.com");
+        assertEquals(expectLink, MarkdownParse.getLinks(contents));
     }
 
     @Test
-    public void testbreakFile() throws IOException {
-        String contents = Files.readString(Path.of("./break-file.md"));
-        List<String> expectLink = List.of("https://something.com", "some-page.html");
-        assertEquals(MarkdownParse.getLinks(contents), expectLink);
-    }
-
-    @Test
-    public void testbreakFileTwo() throws IOException {
-        String contents = Files.readString(Path.of("./break-file2.md"));
-        List<String> expectLink = List.of("https://something.com", "some-page.html");
-        assertEquals(MarkdownParse.getLinks(contents), expectLink);
-    }
-
-    @Test
-    public void testbreakFileThree() throws IOException {
-        String contents = Files.readString(Path.of("./break-file3.md"));
-        List<String> expectLink = List.of("hello.com");
-        assertEquals(MarkdownParse.getLinks(contents), expectLink);
-    }
-
-    @Test
-    public void testbreakFileFour() throws IOException {
-        String contents = Files.readString(Path.of("./break-file4.md"));
-        ArrayList<String> emptyList = new ArrayList<String>();
-        assertEquals(MarkdownParse.getLinks(contents), emptyList);
+    public void testSnippet3() throws IOException {
+        String contents = Files.readString(Path.of("./Snippet3.md"));
+        List<String> expectLink = List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/",
+                "https://cse.ucsd.edu/");
+        assertEquals(expectLink, MarkdownParse.getLinks(contents));
     }
 
 }
+
+/**
+ * @Test
+ *       public void addition() {
+ *       assertEquals(2, 1 + 1);
+ *       }
+ * @Test
+ *       public void getLinks() throws IOException {
+ *       String contents = Files.readString(Path.of("./test-file.md"));
+ *       List<String> expectLink = List.of("https://something.com",
+ *       "some-page.html");
+ *       assertEquals(MarkdownParse.getLinks(contents), expectLink);
+ *       }
+ * 
+ * @Test
+ *       public void testbreakFile() throws IOException {
+ *       String contents = Files.readString(Path.of("./break-file.md"));
+ *       List<String> expectLink = List.of("https://something.com",
+ *       "some-page.html");
+ *       assertEquals(MarkdownParse.getLinks(contents), expectLink);
+ *       }
+ * 
+ * @Test
+ *       public void testbreakFileTwo() throws IOException {
+ *       String contents = Files.readString(Path.of("./break-file2.md"));
+ *       List<String> expectLink = List.of("https://something.com",
+ *       "some-page.html");
+ *       assertEquals(MarkdownParse.getLinks(contents), expectLink);
+ *       }
+ */
